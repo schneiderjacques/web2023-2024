751b14555120e2bd6bc2dc9d83153fdeede264c1
zinedine <zine>
2023-10-20 21:07:13 +0200

    Map positioned automoatically in the user position

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -2,6 +2,8 @@ import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
 import { environment  } from '../../environements/environement';
+import { LocationService } from '../shared/services/location.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-map',
@@ -11,10 +13,10 @@ import { environment  } from '../../environements/environement';
 export class MapComponent implements AfterViewInit {
   private map! : L.Map;
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      //Use env variables here
-      center: environment.mapConfig.center,
+  private initMap(position : L.LatLngTuple): void {
+
+    this.map = L.map('map', {   // get the default location in the env
+      center: position ,
       zoom: environment.mapConfig.defaultZoom
     });
 
@@ -26,9 +28,17 @@ export class MapComponent implements AfterViewInit {
     tiles.addTo(this.map);
   }
 
-  constructor() { }
+  constructor(private _locationService : LocationService) { }
 
   ngAfterViewInit(): void {
-    this.initMap();
+    this._locationService.getLocation().subscribe(
+      (position : L.LatLngTuple ) => {
+        this.initMap(environment.mapConfig.center);
+      },
+      (error) => {
+        this.initMap(environment.mapConfig.center);
+      }
+    )
+
   }
 }
